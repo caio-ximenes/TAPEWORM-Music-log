dabd00cf92ec372fb955f98c08bd1fc648363081
Caio <caio.ximenesmg@gmail.com>
2026-01-16 19:01:19 -0300

    feat:album card

---
+++ src/components/albuns/AlbumCard/AlbumCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Styles from './AlbumCard.module.scss'
+
+function AlbumCard(imgSrc, albumName, artistName) {
+  return (
+    <div className={Styles['album-card']}>
+        <img src={imgSrc} alt="" className={Styles['album-card__image']}/>
+      <div className={Styles['album-card__info']}>
+        <h3 className={Styles['album-card__info__title']}>{albumName}</h3>
+        <p className={Styles['album-card__info__artist']}>{artistName}</p>
+      </div>
+    </div>
+  )
+}
+
+export default AlbumCard
