197abd35ad731f17032f1ecccd8b3d6dcdd5138d
Caio <caio.ximenesmg@gmail.com>
2026-01-16 22:58:40 -0300

    fix: size of card and texts

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import {createBrowserRouter,RouterProvider } from 'react-router-dom'
-import PageHeader from './components/shared/PageHeader/PageHeader.jsx'
+
+
 
 function App() {
     const router = createBrowserRouter([
-      
+      {
+        path:'/',
+      }
     
     ])
 
--- src/components/albuns/AlbumCard/AlbumCard.jsx
+++ src/components/albuns/AlbumCard/AlbumCard.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Styles from './AlbumCard.module.scss'
 
-function AlbumCard(imgSrc, albumName, artistName) {
+function AlbumCard({imgSrc, albumName, artistName}) {
   return (
     <div className={Styles['album-card']}>
-        <img src={imgSrc} alt="" className={Styles['album-card__image']}/>
+        <img src={imgSrc} alt={albumName} className={Styles['album-card__image']}/>
       <div className={Styles['album-card__info']}>
         <h3 className={Styles['album-card__info__title']}>{albumName}</h3>
         <p className={Styles['album-card__info__artist']}>{artistName}</p>
