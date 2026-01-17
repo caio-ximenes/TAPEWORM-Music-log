f52cd722b2b7dbde5d9427fa5d229d4bd3258065
Caio <caio.ximenesmg@gmail.com>
2026-01-16 22:45:58 -0300

    feat:section part added

---
+++ src/components/ui/SectionPart/SectionPart.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Styles from './SectionPart.module.scss'
+
+function SectionPart(lineWidth,text) {
+  return (
+    <div className={Styles['section-part']}>
+        <div className={Styles['section-part__first-line']}></div>
+        <div className={Styles['section-part__text']}>{text}</div>
+        <div className={Styles['section-part__second-line']}></div>
+        <div className={Styles['section-part__circle']}></div>
+    </div>
+  )
+}
+
+export default SectionPart
