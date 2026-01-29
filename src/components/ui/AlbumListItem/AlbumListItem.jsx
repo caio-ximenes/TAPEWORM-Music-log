ea8b27a6091fa7055005dfa3952042b2c74889a5
Caio <caio.ximenesmg@gmail.com>
2026-01-29 17:38:57 -0300

    feat: album list item added

--- src/components/ui/AlbumListItem/AlbumListItem.jsx
+++ src/components/ui/AlbumListItem/AlbumListItem.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Styles from './AlbumListItem.module.scss'
 
 
 
-function AlbumListItem() {
+function AlbumListItem({ order, name,artist, photo, year, stats}) {
+    const [formattedLikes, setFormattedLikes] = useState('0');
+    const [formattedSpins, setFormattedSpins] = useState('0');
+    const rateLevel = {
+    '--rate-level':`${stats.rate}%`
+  }
+    
+
+    useEffect(() => {
+        if (stats.likes === undefined) return;
+        if (stats.spins === undefined) return;
+
+        
+
+        
+        const formatter = new Intl.NumberFormat('en-US', {
+            notation: "compact",
+            compactDisplay: "short",
+            maximumFractionDigits: 1
+        });
+
+        setFormattedLikes(formatter.format(stats.likes).toLowerCase());
+        setFormattedSpins(formatter.format(stats.spins).toLowerCase());
+    }, [stats.likes,stats.spins]);
+    
   return (
     <li className={Styles['album-list-item']}>
-        <div className={Styles['album-list-item__album-stats']}>album stats</div>
-        <div className={Styles['album-list-item__album-info']}>album info</div>
+        <div className={Styles['album-list-item__album-info']}>
+            <img src={photo} alt={name} className={Styles['album-list-item__album-info__photo']} />
+            <ul className={Styles['album-list-item__album-info__data']}>
+                <li className={Styles['album-list-item__album-info__data__order']}>{`${order}.`}</li>
+                <li className={Styles['album-list-item__album-info__data__name']}>{name}</li>
+                <li className={Styles['album-list-item__album-info__data__artist']}>{artist}</li>
+                <li className={Styles['album-list-item__album-info__data__year']}>{year}</li>
+            </ul>
+        </div>
+        <ul className={Styles['album-list-item__album-stats']}>
+            <li className={Styles['album-list-item__album-stats__likes']} id='likes'>
+                {formattedLikes}
+                <label htmlFor='likes' className={Styles['album-list-item__album-stats__likes__label']}>likes</label>
+            </li>
+            <li className={Styles['album-list-item__album-stats__rate']} style={rateLevel}>
+                {stats.rate}
+                <span className={Styles['album-list-item__album-stats__rate__line']}></span>
+            </li>
+            <li className={Styles['album-list-item__album-stats__spin']} id='spins'>
+                {formattedSpins}
+                <label htmlFor='spins' className={Styles['album-list-item__album-stats__spin__label']}>spins</label>
+
+            </li>
+        </ul>
     </li>
   )
 }
--- src/pages/Best/Best.jsx
+++ src/pages/Best/Best.jsx
@@ -4,9 +4,17 @@ import EraSelection from '../../components/ui/EraSelection/EraSelection'
 import AlbumListItem from '../../components/ui/AlbumListItem/AlbumListItem'
 
 function Best() {
+  const status = {likes:12000,rate:92,spins:15000}
   return (
     <>
-      <AlbumListItem />
+      <AlbumListItem 
+      order={1}
+      photo={'https://picsum.photos/200'}
+      name={'The Dark Side Of The Moon'}
+      artist={'caio'}
+      year={'2023'}
+      stats={status}
+       />
       
     </>
   )
