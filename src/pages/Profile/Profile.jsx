322c1edd12f7f9e38bfcfaa5aa7fbaf2872b4137
Caio Ximenes <caio.ximenesmg@gmail.com>
2026-02-04 18:24:45 -0300

    feat: profile page added

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,22 +1,32 @@
-import React, { useRef } from 'react'
-import ProfileSection from '../../components/ui/ProfileSection/ProfileSection'
-import Styles from './Profile.module.scss'
-import PageHeader from '../../components/shared/PageHeader/PageHeader'
-import TextArea from '../../components/ui/TextArea/TextArea'
-import SectionPartition from '../../components/ui/SectionPartition/SectionPartition'
-import RollSection from '../../components/ui/RollSection/RollSection'
-import AlbumCard from '../../components/albuns/AlbumCard/AlbumCard'
-import ArtistCard from '../../components/artists/ArtistCard/ArtistCard'
-import UserData from '../../components/ui/UserData/UserData'
-
-
+import React, { useRef, useEffect, useState } from "react";
+import ProfileSection from "../../components/ui/ProfileSection/ProfileSection";
+import Styles from "./Profile.module.scss";
+import PageHeader from "../../components/shared/PageHeader/PageHeader";
+import TextArea from "../../components/ui/TextArea/TextArea";
+import SectionPartition from "../../components/ui/SectionPartition/SectionPartition";
+import RollSection from "../../components/ui/RollSection/RollSection";
+import AlbumCard from "../../components/albuns/AlbumCard/AlbumCard";
+import ArtistCard from "../../components/artists/ArtistCard/ArtistCard";
+import UserData from "../../components/ui/UserData/UserData";
+import ButtonsHeader from "../../components/ui/ButtonsHeader/ButtonsHeader";
+import RealeaseIcon from "../../components/albuns/RealeaseIcon/RealeaseIcon";
 
 function Profile() {
+  const [userLists,setUserLists] = useState([]);
+  const [list, setList] = useState([]);
+
+  const buttons = [
+    { id: 0, name: "spun", onclick: filterLists },
+    { id: 1, name: "liked", onclick: filterLists },
+    { id: 2, name: "essays", onclick: filterLists },
+    { id: 3, name: "spin list", onclick: filterLists },
+  ];
+
   // Exemplo de dados dinâmicos
   const userStats = [
-    { name: 'Seguidores', value: 12000 },
-    { name: 'Seguindo', value: 450 },
-    { name: 'Playlists', value: 12 },
+    { name: "Seguidores", value: 12000 },
+    { name: "Seguindo", value: 450 },
+    { name: "Playlists", value: 12 },
   ];
 
   // Mock data function for artists
@@ -67,7 +77,7 @@ function Profile() {
         id: 10,
         artistName: "Retro Wave",
         srcImage: "https://picsum.photos/200",
-      }
+      },
     ];
     return artists;
   }
@@ -134,76 +144,252 @@ function Profile() {
         albumName: "Vintage Vibes",
         artistName: "Retro Wave",
         imgSrc: "https://picsum.photos/200",
-      }
+      },
     ];
     return albums;
   }
-  
+
+  function getUserLists() {
+    const mockData = [
+      {
+        id: "spun",
+        list: [
+          {
+            img: "https://picsum.photos/200?random=1",
+            name: "Spun Album 1",
+            rate: 85,
+          },
+          {
+            img: "https://picsum.photos/200?random=2",
+            name: "Spun Album 2",
+            rate: 92,
+          },
+        ],
+      },
+      {
+        id: "liked",
+        list: [
+          {
+            img: "https://picsum.photos/200?random=3",
+            name: "Liked Song 1",
+            rate: 100,
+          },
+          {
+            img: "https://picsum.photos/200?random=4",
+            name: "Liked Song 2",
+            rate: 95,
+          },
+        ],
+      },
+      {
+        id: "essays",
+        list: [
+          {
+            img: "https://picsum.photos/200?random=5",
+            name: "Essay Title 1",
+            rate: 88,
+          },
+          {
+            img: "https://picsum.photos/200?random=6",
+            name: "Essay Title 2",
+            rate: 75,
+          },
+        ],
+      },
+      {
+        id: "spinlist",
+        list: [
+          {
+            img: "https://picsum.photos/200?random=7",
+            name: "Spin List Item 1",
+            rate: 0,
+          },
+          {
+            img: "https://picsum.photos/200?random=8",
+            name: "Spin List Item 2",
+            rate: 0,
+          },{
+            img: "https://picsum.photos/200?random=7",
+            name: "Spin List Item 1",
+            rate: 0,
+          },
+          {
+            img: "https://picsum.photos/200?random=8",
+            name: "Spin List Item 2",
+            rate: 0,
+          },{
+            img: "https://picsum.photos/200?random=7",
+            name: "Spin List Item 1",
+            rate: 0,
+          },
+          {
+            img: "https://picsum.photos/200?random=8",
+            name: "Spin List Item 2",
+            rate: 0,
+          },{
+            img: "https://picsum.photos/200?random=7",
+            name: "Spin List Item 1",
+            rate: 0,
+          },
+          {
+            img: "https://picsum.photos/200?random=8",
+            name: "Spin List Item 2",
+            rate: 0,
+          },{
+            img: "https://picsum.photos/200?random=7",
+            name: "Spin List Item 1",
+            rate: 0,
+          },
+          {
+            img: "https://picsum.photos/200?random=8",
+            name: "Spin List Item 2",
+            rate: 0,
+          },{
+            img: "https://picsum.photos/200?random=7",
+            name: "Spin List Item 1",
+            rate: 0,
+          },
+          {
+            img: "https://picsum.photos/200?random=8",
+            name: "Spin List Item 2",
+            rate: 0,
+          },{
+            img: "https://picsum.photos/200?random=7",
+            name: "Spin List Item 1",
+            rate: 0,
+          },
+          {
+            img: "https://picsum.photos/200?random=8",
+            name: "Spin List Item 2",
+            rate: 0,
+          },{
+            img: "https://picsum.photos/200?random=7",
+            name: "Spin List Item 1",
+            rate: 0,
+          },
+          {
+            img: "https://picsum.photos/200?random=8",
+            name: "Spin List Item 2",
+            rate: 0,
+          },{
+            img: "https://picsum.photos/200?random=7",
+            name: "Spin List Item 1",
+            rate: 0,
+          },
+          {
+            img: "https://picsum.photos/200?random=8",
+            name: "Spin List Item 2",
+            rate: 0,
+          },{
+            img: "https://picsum.photos/200?random=7",
+            name: "Spin List Item 1",
+            rate: 0,
+          },
+          {
+            img: "https://picsum.photos/200?random=8",
+            name: "Spin List Item 2",
+            rate: 0,
+          },
+        ],
+      },
+    ];
+
+    setUserLists(mockData);
+    return mockData;
+  }
+
+  function filterLists(section) {
+    if (userLists[section]) {
+      if(userLists.length > 0) {
+      setList(userLists[section].list
+      );
+      }
+    }
+  }
+
+ useEffect(() => {
+  const data = getUserLists();
+  const spun = data.find(item => item.id === "spun");
+  setList(spun.list);
+}, []);
+
   return (
     <>
-    <PageHeader />
-    <main className={Styles['profile']}>
-      <div className={Styles['profile__main-data']}>
-        <section className={Styles['profile__first-column']}>
-          <div className={Styles['profile__profile-section']}>
-            <ProfileSection
-              name={'Kiur086'} />
-          </div>
-          <div className={Styles['profile__bio']} >
-            <SectionPartition text={'Bio'}
-              width={'100%'}
-            />
-            <TextArea
-              text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'} />
-
-          </div>
-        </section>
-        <section className={Styles['profile__second-column']}>
-          <div className={Styles['profile__stats']}>
-            <UserData stats={userStats} />
-          </div>
-          <div className={Styles['profile__artists']}>
-            <div className={Styles['profile__artists-header']}>
-                <SectionPartition
-                text={'favourite artists'}
-                width={'100%'} />
+      <PageHeader />
+      <main className={Styles["profile"]}>
+        <div className={Styles["profile__main-data"]}>
+          <section className={Styles["profile__first-column"]}>
+            <div className={Styles["profile__profile-section"]}>
+              <ProfileSection name={"Kiur086"} />
+            </div>
+            <div className={Styles["profile__bio"]}>
+              <SectionPartition text={"Bio"} width={"100%"} />
+              <TextArea
+                text={
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                }
+              />
+            </div>
+          </section>
+          <section className={Styles["profile__second-column"]}>
+            <div className={Styles["profile__stats"]}>
+              <UserData stats={userStats} />
+            </div>
+            <div className={Styles["profile__artists"]}>
+              <div className={Styles["profile__artists-header"]}>
+                <SectionPartition text={"favourite artists"} width={"100%"} />
               </div>
-            <RollSection>
-              {getArtists().map((artist) => (
-                <ArtistCard
-                  key={artist.id}
-                  srcImage={artist.srcImage}
-                  artistName={artist.artistName}
-                />
-              ))}
-            </RollSection>
-
-          </div>
-          <div className={Styles['profile__albums']}>
-            <div className={Styles['profile__albums-header']}>
-              <SectionPartition
-                text={'favourite releases'}
-                width={'100%'} />
+              <RollSection>
+                {/* Retrieve artist data and map each artist to an ArtistCard component */}
+                {getArtists().map((artist) => (
+                  <ArtistCard
+                    key={artist.id}
+                    srcImage={artist.srcImage}
+                    artistName={artist.artistName}
+                  />
+                ))}
+              </RollSection>
+            </div>
+            <div className={Styles["profile__albums"]}>
+              <div className={Styles["profile__albums-header"]}>
+                <SectionPartition text={"favourite releases"} width={"100%"} />
               </div>
-            <RollSection className={Styles['profile__albums-list']}>
-              {getReleases().map((album) => (
-                <AlbumCard
-                  key={album.id}
-                  imgSrc={album.imgSrc}
-                  albumName={album.albumName}
-                  artistName={album.artistName}
-                />
-              ))}
-            </RollSection>
-
-
+              <RollSection className={Styles["profile__albums-list"]}>
+                {/* Retrieve album data and map each album to an AlbumCard component */}
+                {getReleases().map((album) => (
+                  <AlbumCard
+                    key={album.id}
+                    imgSrc={album.imgSrc}
+                    albumName={album.albumName}
+                    artistName={album.artistName}
+                  />
+                ))}
+              </RollSection>
+            </div>
+          </section>
+        </div>
+        <section className={Styles["profile__user-lists"]}>
+          <div
+            className={Styles["profile__user-lists__header"]}
+            aria-label="header for user lists with buttons"
+          >
+            <ButtonsHeader buttons={buttons} />
           </div>
+          <div className={Styles["profile__user-lists__list"]}>
+            {list.map((item) => (
+              <RealeaseIcon
+                key={item.id}
+                img={item.img}
+                name={item.name}
+                rate={item.rate}
+              />
+            ))}
+          </div>
+          
         </section>
-      </div>
-      <div className={Styles['profile__user-lists']}></div>
-    </main>
+      </main>
     </>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
