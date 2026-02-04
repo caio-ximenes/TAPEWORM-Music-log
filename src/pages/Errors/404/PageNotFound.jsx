f0264f3e0c2f92c5e1491f346a6b9537c4dc6a44
Caio Ximenes <caio.ximenesmg@gmail.com>
2026-02-04 18:23:23 -0300

    style: 404 page beautyfull

--- src/pages/Errors/404/PageNotFound.jsx
+++ src/pages/Errors/404/PageNotFound.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { Link,useNavigate } from 'react-router-dom'
+import TapeBreak from '../../../assets/animations/TapeBreak/TapeBreak';
+import Styles from './PageNotFound.module.scss'
 
 function PageNotFound() {
 const navigate = useNavigate();
 
   return (
   <>
-    <div>
-        Oops... ainda não temos o que você procura.
-    </div>
-    <Link to={navigate(-1)}>Voltar</Link>
+    <main className={Styles['page-not-found']}>
+      <h1 className={Styles['page-not-found__title']}>
+          Oops... ainda não temos o que você procura.
+      </h1>
+      <Link to={navigate(-1)} className={Styles['page-not-found__link']}>
+        Voltar
+        <div className={Styles['page-not-found__link__arrow']}></div>
+      </Link>
+      <TapeBreak />
+    </main>
   </>
   )
 }
