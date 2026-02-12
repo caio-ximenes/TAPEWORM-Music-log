5df4a6fb5cf358be44467a722a7036301b413ca1
Caio Ximenes <caio.ximenesmg@gmail.com>
2026-02-11 22:30:52 -0300

    refact: realease icon can show diferent stats if they were passed

--- src/components/albuns/RealeaseIcon/RealeaseIcon.jsx
+++ src/components/albuns/RealeaseIcon/RealeaseIcon.jsx
@@ -1,39 +1,80 @@
-import React from 'react'
-import Styles from './RealeaseIcon.module.scss'
+import React, { use } from "react";
+import Styles from "./RealeaseIcon.module.scss";
+import { useShortFormatter } from "../../../hooks/useShortFormatter";
+import { useImageContrast } from "../../../hooks/useImageContrast";
 
-
-function RealeaseIcon({key,img, name, rate, onClick,stats}) {
-
+function RealeaseIcon({
+  key,
+  img,
+  name,
+  rate = null,
+  stats = null,
+  onClick = null,
+  play = "play",
+}) {
   const rateLevel = {
     "--rate-level": `${rate}%`,
   };
 
-  return (
-    <li className={Styles['realease-icon']} key={key}>
-        <div className={Styles['realease-icon__image-container']}>
-            <img src={img} alt={name} className={Styles['realease-icon__image-container__image']} />
-            <div className={Styles['realease-icon__image-container__rate']}>{rate}
-              <div className={Styles['realease-icon__image-container__rate-line']} style={rateLevel}></div>
-            </div>
-            
-        </div>
-        <div className={Styles['realease-icon__name']}>{name}</div>
-        <div className={Styles['realease-icon__stats']}>
-          <div className={Styles['realease-icon__stats__item']}>
-            <div className={Styles['realease-icon__stats__item__spins']}></div>
-            {stats.spins}
-            </div>
-          <div className={Styles['realease-icon__stats__item']}>
-            <div className={Styles['realease-icon__stats__item__likes']}></div>
-            {stats.likes}
-            </div>
-          <div className={Styles['realease-icon__stats__item']}>
-            <div className={Styles['realease-icon__stats__item__favorites']}></div>
-            {stats.favorites}</div>
+  const hover = onClick
+    ? null
+    : {
+        "transform": "none",
+        "cursor": "default",
+      };
 
+
+      const buttonColor = useImageContrast(img);
+
+      
+
+  const formattedStats = useShortFormatter(stats);
+
+  return (
+    <li
+      className={Styles["realease-icon"]}
+      key={key}
+      style={hover}
+      onClick={onClick}
+    >
+      <div className={Styles["realease-icon__image-container"]}>
+        <img
+          src={img}
+          alt={name}
+          className={Styles["realease-icon__image-container__image"]}
+        />
+        <div className={Styles["realease-icon__image-container__play"]} style={{ backgroundColor: buttonColor }}></div>
+        {rate && (
+          <div className={Styles["realease-icon__image-container__rate"]}>
+            {rate}
+            <div
+              className={Styles["realease-icon__image-container__rate-line"]}
+              style={rateLevel}
+            ></div>
+          </div>
+        )}
+      </div>
+      {stats && (
+        <div className={Styles["realease-icon__stats"]}>
+          <div className={Styles["realease-icon__stats__item"]}>
+            <div className={Styles["realease-icon__stats__item__spins"]}></div>
+            {formattedStats.spins ? formattedStats.spins : 0}
+          </div>
+          <div className={Styles["realease-icon__stats__item"]}>
+            <div className={Styles["realease-icon__stats__item__likes"]}></div>
+            {formattedStats.likes ? formattedStats.likes : 0}
+          </div>
+          <div className={Styles["realease-icon__stats__item"]}>
+            <div
+              className={Styles["realease-icon__stats__item__favorites"]}
+            ></div>
+            {formattedStats.favorites ? formattedStats.favorites : 0}
+          </div>
         </div>
+      )}
+      <div className={Styles["realease-icon__name"]}>{stats ? null : name}</div>
     </li>
-  )
+  );
 }
 
-export default RealeaseIcon
+export default RealeaseIcon;
