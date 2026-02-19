968956e978143db42d3805d2a9496c0ac10f8e1d
Caio Ximenes <caio.ximenesmg@gmail.com>
2026-02-19 12:02:09 -0300

    refact,styles: made more dynamic and animated

--- src/components/ui/BooleanIconButton/BooleanIconButton.jsx
+++ src/components/ui/BooleanIconButton/BooleanIconButton.jsx
@@ -1,4 +1,4 @@
-import { button } from "framer-motion/client";
+import { motion } from "framer-motion";
 import React from "react";
 import Styles from "./BooleanIconButton.module.scss";
 
@@ -7,23 +7,33 @@ import Styles from "./BooleanIconButton.module.scss";
 // For the component to work, you must place the icon name in the variables.scss file following the pattern:
 // icon-name-state(empty or full): url("../assets/images/icons/icon-name-state.svg");
 // Then make sure the name you gave is being passed as a prop named "icon" to the button
+// This component receives a function as a prop named "onClick" that will be executed when the button is clicked, is the button feature
+// this feature will be controlled by the father component
+// It's a controlled component, it receives its state as a prop named "buttonState" and the father component is responsible for managing this state and passing the correct value to the button
 
-function BooleanIconButton({ icon, name }) {
-  const [isActive, setIsActive] = React.useState(false);
+function BooleanIconButton({icon,name,onClick = null,buttonState}) {
 
-  const state = isActive ? "full" : "empty";
+  const state = buttonState ? "full" : "empty";
 
   const iconClass = `
     ${Styles["boolean-icon-button__icon"]}
     ${Styles[`boolean-icon-button__icon--${icon}-${state}`]}
   `;
+  
 
   return (
     <button
-      onClick={() => setIsActive(!isActive)}
+      onClick={() => {onClick && onClick(!buttonState);
+      }}
       className={Styles["boolean-icon-button"]}
     >
-      <div className={iconClass}></div>
+      <motion.div
+        key={state}
+        initial={{ scale: 0.5, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ type: "spring", stiffness: 300, damping: 20 }}
+        className={iconClass}
+      />
       <p className={Styles["boolean-icon-button__name"]}>{name}</p>
     </button>
   );
