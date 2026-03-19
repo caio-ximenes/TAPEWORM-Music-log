328a4f39f01ee6f6deccc76f7eb0bfc47115f2cc
Caio Ximenes <caio.ximenesmg@gmail.com>
2026-03-18 22:34:38 -0300

    fix: empty space at the botton of the boolean button even the text dont exists

--- src/components/ui/BooleanIconButton/BooleanIconButton.jsx
+++ src/components/ui/BooleanIconButton/BooleanIconButton.jsx
@@ -34,7 +34,7 @@ function BooleanIconButton({icon,name,onClick = null,buttonState}) {
         transition={{ type: "spring", stiffness: 300, damping: 20 }}
         className={iconClass}
       />
-      <p className={name ? Styles["boolean-icon-button__name"]:{}}>{name}</p>
+      {name && <p className={name ? Styles["boolean-icon-button__name"]:{}}>{name}</p>}
     </button>
   );
 }
